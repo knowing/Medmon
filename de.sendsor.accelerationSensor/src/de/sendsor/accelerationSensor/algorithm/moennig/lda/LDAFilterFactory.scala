2319159ffa970c4ae1f13935e7032bc0f15cb6c4
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2011-08-26 14:50:26 +0200

    Little modification in LDAFactory. isBuild flag is being set correctly to LDA function

--- de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/algorithm/moennig/lda/LDAFilterFactory.scala
+++ de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/algorithm/moennig/lda/LDAFilterFactory.scala
@@ -5,13 +5,13 @@ import scala.collection.immutable.Map
 import akka.actor.Actor
 import de.lmu.ifi.dbs.knowing.core.factory._
 import de.lmu.ifi.dbs.knowing.core.factory.TFactory._
-import de.lmu.ifi.dbs.knowing.core.weka.{WekaFilter, WekaFilterFactory }
+import de.lmu.ifi.dbs.knowing.core.weka.{ WekaFilter, WekaFilterFactory }
 import de.lmu.ifi.dbs.knowing.core.weka.WekaFilterFactory._
 import LDAFilterFactory._
 import weka.core.Instances
 
 class LDAFilterFactory extends WekaFilterFactory[LDAFilterWrapper, LDAFilter](classOf[LDAFilterWrapper], classOf[LDAFilter]) {
-  
+
   //Creates default Properties which are used if properties aren't set
   override def createDefaultProperties: Properties = {
     val returns = new Properties
@@ -22,46 +22,49 @@ class LDAFilterFactory extends WekaFilterFactory[LDAFilterWrapper, LDAFilter](cl
   }
 
   //Possible property values. Editor should show these
-  override def createPropertyValues: Map[String, Array[_<:Any]] = {
+  override def createPropertyValues: Map[String, Array[_ <: Any]] = {
     Map(DEBUG -> boolean_property)
     Map(DIMREDUCTION -> boolean_property)
   }
 
   //Property description
   override def createPropertyDescription: Map[String, String] = {
-        Map(DEBUG -> "Debug true/false")
-        Map(DIMREDUCTION -> "Dimension reduction true/false")        
+    Map(DEBUG -> "Debug true/false")
+    Map(DIMREDUCTION -> "Dimension reduction true/false")
   }
-
 
 }
 
-object LDAFilterFactory{
+object LDAFilterFactory {
   val DIMREDUCTION = "dimensionreduction"
   val OUTDIMENSIONS = "outputdimensions"
   val ATTRIBUTE_NAME_PREFIX = "attributeNamePrefix"
 }
 
 class LDAFilterWrapper extends WekaFilter(new LDAFilter()) {
-  
-   override def build(instances: Instances) {
-	   val lda = filter.asInstanceOf[LDAFilter]
-	   guessAndSetClassLabel(instances)
-	   lda.train(instances)
-   }
-  
-   override def configure(properties:Properties) = {
-     //Configure your classifier here with
-     val lda = filter.asInstanceOf[LDAFilter]
-     val debug = properties.getProperty(DEBUG)
-     val dimreduction = properties.getProperty(DIMREDUCTION)
-     val outdimensions = properties.getProperty(OUTDIMENSIONS)
-     val attNamePrefix = properties.getProperty(ATTRIBUTE_NAME_PREFIX);
-     lda.setDebug(debug.toBoolean)
-     lda.setDimensionReduction(dimreduction.toBoolean)
-     if(outdimensions!=null){
-    	 lda.setOutDimensions(outdimensions.toInt)
-     }
-     lda.setAttributeNamePrefix(attNamePrefix)
-   }
+
+  //LDA must be trained, before queries are being answered
+  isBuild = false
+
+  override def build(instances: Instances) {
+    val lda = filter.asInstanceOf[LDAFilter]
+    guessAndSetClassLabel(instances)
+    lda.train(instances)
+    isBuild = true
+  }
+
+  override def configure(properties: Properties) = {
+    //Configure your classifier here with
+    val lda = filter.asInstanceOf[LDAFilter]
+    val debug = properties.getProperty(DEBUG)
+    val dimreduction = properties.getProperty(DIMREDUCTION)
+    val outdimensions = properties.getProperty(OUTDIMENSIONS)
+    val attNamePrefix = properties.getProperty(ATTRIBUTE_NAME_PREFIX);
+    lda.setDebug(debug.toBoolean)
+    lda.setDimensionReduction(dimreduction.toBoolean)
+    if (outdimensions != null) {
+      lda.setOutDimensions(outdimensions.toInt)
+    }
+    lda.setAttributeNamePrefix(attNamePrefix)
+  }
 }
