55af837eace89e73518f3271784fc5174a9a6e2a
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2011-12-08 12:23:31 +0100

    * Changed .gitignore in de.sendsor plugin * Removed old testdata * SDRLoader now uses TStreamResolver

--- de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/SDRLoader.scala
+++ de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/SDRLoader.scala
@@ -16,43 +16,48 @@ import weka.core.Instances
 
 /**
  * @author Nepomuk Seiler
- * @version 0.1
+ * @version 0.1.1
  * @since 08.05.2011
  *
  */
 class SDRLoader extends TLoader {
 
-  var converter: SDRConverter = _
+  private[this] var average = AGGREGATE_NONE
+  private[this] var interval = INTERVAL_SECOND
+  private[this] var units = 1.0
+  private[this] var timestamp = RELATIVE_TIMESTAMP_RELATIVE
 
-  def getDataSet(): Instances = converter.getData
-
-  def configure(properties: Properties) = {
-    val path = TLoader.getFilePath(properties)
-    converter = new SDRConverter(path)
-    
-    val average = properties.getProperty(AGGREGATE, AGGREGATE_AVERAGE)
-    if(AGGREGATE_PROPERTIES contains(average)) {
-      converter.setAggregate(average)
-    }
-    
-    val interval = properties.getProperty(INTERVAL, INTERVAL_SECOND)
-    if(INTERVAL_PROPERTIES contains(interval)) {
-      converter.setInterval(interval)
-    }
-    
-    val units = properties.getProperty(UNITS, "1.0")
+  def getDataSet(): Instances = {
+    val converter = new SDRConverter(inputs.values.head)
+    converter.setAggregate(average)
+    converter.setInterval(interval)
     converter.setUnits(units.toDouble)
-    
-    val timestamp = properties.getProperty(RELATIVE_TIMESTAMP)
     timestamp match {
       case RELATIVE_TIMESTAMP_ABSOLUTE => converter.setRelativeTimestamp(false)
       case RELATIVE_TIMESTAMP_RELATIVE => converter.setRelativeTimestamp(true)
-      case _ => converter.setRelativeTimestamp(false) //Print warning
+      case _ => converter.setRelativeTimestamp(true) //Print warning
     }
-    
+
+    converter.getData
   }
 
-  def reset = converter.reset()
+  def configure(properties: Properties) {
+
+    val average = properties.getProperty(AGGREGATE, AGGREGATE_NONE)
+    if (AGGREGATE_PROPERTIES contains (average)) {
+      this.average = average
+    }
+
+    val interval = properties.getProperty(INTERVAL, INTERVAL_SECOND)
+    if (INTERVAL_PROPERTIES contains (interval)) {
+      this.interval = interval
+    }
+    val units = properties.getProperty(UNITS, "1.0")
+    val timestamp = properties.getProperty(RELATIVE_TIMESTAMP)
+
+  }
+
+  def reset = {}
 }
 
 class SDRLoaderFactory extends ProcessorFactory(classOf[SDRLoader]) {
@@ -61,24 +66,24 @@ class SDRLoaderFactory extends ProcessorFactory(classOf[SDRLoader]) {
     val props = new Properties
     props.setProperty(FILE, "")
     props.setProperty(URL, "")
-    props.setProperty(AGGREGATE, AGGREGATE_AVERAGE )
+    props.setProperty(AGGREGATE, AGGREGATE_NONE)
     props.setProperty(INTERVAL, INTERVAL_SECOND)
     props.setProperty(UNITS, "1")
-    props.setProperty(RELATIVE_TIMESTAMP,  RELATIVE_TIMESTAMP_ABSOLUTE)
+    props.setProperty(RELATIVE_TIMESTAMP, RELATIVE_TIMESTAMP_ABSOLUTE)
     props
   }
 
   override def createPropertyValues(): Map[String, Array[Any]] = {
-    Map(AGGREGATE -> AGGREGATE_PROPERTIES.toArray ,
-        INTERVAL -> INTERVAL_PROPERTIES.toArray ,
-        RELATIVE_TIMESTAMP -> Array( RELATIVE_TIMESTAMP_RELATIVE,  RELATIVE_TIMESTAMP_ABSOLUTE))
+    Map(AGGREGATE -> AGGREGATE_PROPERTIES.toArray,
+      INTERVAL -> INTERVAL_PROPERTIES.toArray,
+      RELATIVE_TIMESTAMP -> Array(RELATIVE_TIMESTAMP_RELATIVE, RELATIVE_TIMESTAMP_ABSOLUTE))
   }
 
   override def createPropertyDescription(): Map[String, String] = {
     Map(AGGREGATE -> "Aggregate: none|average|interval_first|interval_last .",
-        INTERVAL -> "In which interval should the data be aggregated: second|minute|hour|day",
-        UNITS -> "Must be greater than 0. Determines the interval size (units*interval). E.g 15 minutes",
-        RELATIVE_TIMESTAMP -> "relative | absolute .Relative uses the first timestamp and assumes correct recorded data")
+      INTERVAL -> "In which interval should the data be aggregated: second|minute|hour|day",
+      UNITS -> "Must be greater than 0. Determines the interval size (units*interval). E.g 15 minutes",
+      RELATIVE_TIMESTAMP -> "relative | absolute .Relative uses the first timestamp and assumes correct recorded data")
   }
 
 }
@@ -95,13 +100,13 @@ object SDRLoaderFactory {
   val AGGREGATE_INTERVAL_FIRST = "interval_first"
   val AGGREGATE_INTERVAL_LAST = "interval_last"
   val AGGREGATE_PROPERTIES = Array(AGGREGATE_NONE, AGGREGATE_AVERAGE, AGGREGATE_INTERVAL_FIRST, AGGREGATE_INTERVAL_LAST)
-    
+
   val INTERVAL_SECOND = "second" //Default
   val INTERVAL_MINUTE = "minute"
   val INTERVAL_HOUR = "hour"
   val INTERVAL_DAY = "day"
   val INTERVAL_PROPERTIES = Array(INTERVAL_SECOND, INTERVAL_MINUTE, INTERVAL_HOUR, INTERVAL_DAY)
-  
+
   val RELATIVE_TIMESTAMP_RELATIVE = "relative"
   val RELATIVE_TIMESTAMP_ABSOLUTE = "absolute" //Default
 }
