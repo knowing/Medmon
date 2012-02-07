183fd3a73ce1e6fb7be7715eaca08039367fa490
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-02-07 22:02:09 +0100

    Implemented NormalizeAndroidSensorDataProcessor. -> properties: hz == set frequency strategy == average | first | last (how to deal with multiple values)
    NOTE: not tested yet

--- de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/algorithm/walonka/NormalizeAndroidSensorData.scala
+++ de.sendsor.accelerationSensor/src/de/sendsor/accelerationSensor/algorithm/walonka/NormalizeAndroidSensorData.scala
@@ -4,68 +4,126 @@ import java.util.Properties
 import weka.core.{ Instances, Attribute, DenseInstance, Instance }
 import de.lmu.ifi.dbs.knowing.core.factory.ProcessorFactory
 import de.lmu.ifi.dbs.knowing.core.processing.TFilter
-import de.lmu.ifi.dbs.knowing.core.util.ResultsUtil.findValueAttributesAsMap
+import de.lmu.ifi.dbs.knowing.core.util.ResultsUtil.{ findValueAttributes, findNumericAttributes }
 import de.lmu.ifi.dbs.knowing.core.util.ResultsUtil.ATTRIBUTE_TIMESTAMP
+import scala.collection.JavaConversions._
+import scala.collection.parallel.mutable.ParSeq
+import NormalizeAndroidSensorDataFactory._
 
+/**
+ * Normalize sensor data to a specific frequency.
+ * This processor works for ResultUtils.timeSeriesResult
+ *
+ * @author Nepomuk Seiler
+ * @version 0.1
+ */
 class NormalizeAndroidSensorData extends TFilter {
 
-  /**
-   * Just prints out the map and returns the head of the map or emptyResult
-   */
-  override def filter(instances: Instances): Instances = {
-    //static field
-    val defaultHz = 25
-    
-    val filtered = new Instances(instances, instances.size)
+	private var hz = 25
+	private var strategy = STRATEGY_AVERAGE
 
-    //run it with scala paralell collection
-    val grouped = instances.par groupBy {
-      inst =>
-        val timestamp = inst.value(0).toLong
-        val millis = timestamp % 1000
-        val times = millis / 40 //40ms == 25hz
-        val floor = times * 40
-        val ceil = (times + 1) * 40
+	override def filter(instances: Instances): Instances = {
+		val filtered = new Instances(instances, instances.size)
+		val timeslot = 1000 / hz //in ms
 
-        val FloorVal = millis - floor
-        val CeilVal = ceil - millis
-        val nearest = Math.min(FloorVal, CeilVal)
-        nearest match {
-          case FloorVal => timestamp - FloorVal
-          case CeilVal => timestamp + CeilVal
-        }
-    }
-    //TODO -> check that no gaps in timestamps
+		//careful! If dataset consist of a value y0, it is handled with the first call
+		val valueAttrs = findValueAttributes(instances) match {
+			case attr if attr.isEmpty => findNumericAttributes(instances).toList
+			case attr => attr.toList
+		}
+		val timeAttr = instances.attribute(ATTRIBUTE_TIMESTAMP)
 
-    //average values -> extract in custom method
-    grouped.par foreach {
-      case (timestamp, b) if b.isEmpty => throw new Exception("Empty timeslot")
+		//run it with scala parallel collection
+		val grouped = instances.par groupBy {
+			inst =>
+				val timestamp = inst.value(timeAttr).toLong
+				val millis = timestamp % 1000
+				val times = millis / timeslot
+				val floor = times * timeslot
+				val ceil = (times + 1) * timeslot
 
-      //only one value
-      case (timestamp, b) if b.size == 1 =>
-        val inst = new DenseInstance(b.head)
-        inst.setValue(0, timestamp)
-        filtered.add(inst)
+				val FloorVal = millis - floor
+				val CeilVal = ceil - millis
+				val nearest = Math.min(FloorVal, CeilVal)
+				nearest match {
+					case FloorVal => timestamp - FloorVal
+					case CeilVal => timestamp + CeilVal
+				}
+		}
+		//average values -> extract in custom method
+		grouped.par foreach {
+			case (timestamp, instances) if instances.isEmpty => throw new Exception("Empty timeslot at " + timestamp)
 
-      //average | choose 
-      case (timestamp, b) =>
-        val inst = new DenseInstance(b.head)
-        inst.setValue(0, timestamp)
-        val sum = b.foldLeft(0.0)((sum, inst) => sum + inst.value(1))
-        val avg = sum / b.size
-        inst.setValue(1, avg)
-        filtered.add(inst)
-    }
-    filtered.sort(0)
-    filtered
-  }
+			//only one value
+			case (timestamp, instances) if instances.size == 1 =>
+				val inst = new DenseInstance(instances.head)
+				inst.setValue(timeAttr, timestamp)
+				filtered.add(inst)
 
-  def query(query: Instance): Instances = throw new UnsupportedOperationException
+			//average | choose 
+			case (timestamp, instances) =>
+				val inst = strategy match {
+					case STRATEGY_AVERAGE => average(instances, valueAttrs)
+					case STRATEGY_FIRST => first(instances)
+					case STRATEGY_LAST => last(instances)
+				}
+				inst.setValue(timeAttr, timestamp)
+				filtered.add(inst)
+		}
+		//make sure it's sorted
+		filtered.sort(timeAttr)
+		filtered
+	}
 
-  def result(result: Instances, query: Instance) = {}
+	private def average(instances: ParSeq[Instance], attributes: List[Attribute]): Instance = {
+		val inst = new DenseInstance(instances.head)
+		for (attr <- attributes) {
+			val sum = instances.foldLeft(0.0)((sum, inst) => sum + inst.value(attr))
+			val avg = sum / instances.size
+			inst.setValue(attr, avg)
+		}
+		inst
+	}
 
-  def configure(properties: Properties) = {}
+	private def first(instances: ParSeq[Instance]): Instance = new DenseInstance(instances.head)
+
+	private def last(instances: ParSeq[Instance]): Instance = new DenseInstance(instances.reverse.head)
+
+	def configure(properties: Properties) = {
+		hz = properties.getProperty(HZ, "25").toInt
+		strategy = properties.getProperty(STRATEGY, STRATEGY_AVERAGE)
+	}
+
+	def query(query: Instance): Instances = throw new UnsupportedOperationException
+
+	def result(result: Instances, query: Instance) = {}
 
 }
 
-class NormalizeAndroidSensorDataFactory extends ProcessorFactory(classOf[NormalizeAndroidSensorData])
+class NormalizeAndroidSensorDataFactory extends ProcessorFactory(classOf[NormalizeAndroidSensorData]) {
+
+	override def createDefaultProperties: Properties = {
+		val props = new Properties
+		props.setProperty(HZ, "25")
+		props.setProperty(STRATEGY, STRATEGY_AVERAGE)
+		props
+	}
+
+	override def createPropertyValues: Map[String, Array[_ <: Any]] = Map(
+		STRATEGY -> Array(STRATEGY_AVERAGE, STRATEGY_FIRST, STRATEGY_LAST))
+
+	override def createPropertyDescription: Map[String, String] = Map(
+		HZ -> "Target frequency. Has to be lower than the source frequency",
+		STRATEGY -> "How to deal with multiple values. Average | take first | take last")
+
+}
+
+object NormalizeAndroidSensorDataFactory {
+
+	val HZ = "hz"
+	val STRATEGY = "strategy"
+
+	val STRATEGY_AVERAGE = "average"
+	val STRATEGY_FIRST = "first"
+	val STRATEGY_LAST = "last"
+}
