a9728be99aa56ceda5b1fc661dd50c1df2fc5bf8
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-11-24 13:36:42 +0100

    Derby tests are up and running

--- de.lmu.ifi.dbs.medmon.derby/src/test/java/de/lmu/ifi/dbs/medmon/database/install/DataTest.java
+++ de.lmu.ifi.dbs.medmon.derby/src/test/java/de/lmu/ifi/dbs/medmon/database/install/DataTest.java
@@ -1,6 +1,8 @@
 package de.lmu.ifi.dbs.medmon.database.install;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 
@@ -20,64 +22,71 @@ public class DataTest extends DatabaseTest {
 	public void testPlainData() {
 		Patient patient = createPatient();
 		Sensor sensor = createSensor();
-		
-		//Create Data
+
+		// Create Data
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		Data data = new Data(new Date(0), new Date(10), sensor);
-		
+		data.setPatient(patient);
+		data.setType(Data.RAW);
+
 		assertEquals("Id should not be set", data.getId(), 0);
 		em.persist(data);
 		assertTrue("Id should be set", data.getId() != 0);
 		em.getTransaction().commit();
 		em.close();
-		
-		//Remove Data
+
+		// Remove Data
 		em = emf.createEntityManager();
 		em.getTransaction().begin();
 		Data d = em.find(Data.class, data.getId());
-		assertNotNull("Data not persisted!",d);
-		assertNotNull("Relation to Sensor not persisted",d.getSensor());
-		
+		assertNotNull("Data not persisted!", d);
+		assertNotNull("Relation to Sensor not persisted", d.getSensor());
+
 		assertEquals("Sensors not equally", d.getSensor(), sensor);
-		
+
 		em.remove(d);
 		em.getTransaction().commit();
 		em.close();
-		
+
 		removePatient(patient);
 		removeSensor(sensor);
 	}
-	
+
 	@Test
 	public void testDataRemovalWithTherapyResult() {
 		Patient patient = createPatient();
 		Sensor sensor = createSensor();
-		
-		//Create Data
+
+		// Create Data
 		EntityManager em = emf.createEntityManager();
 		em.getTransaction().begin();
 		Data data = new Data(new Date(0), new Date(10), sensor);
-		
+		data.setPatient(patient);
+		data.setType(Data.RAW);
+
 		assertEquals("Id should not be set", data.getId(), 0);
 		em.persist(data);
 		assertTrue("Id should be set", data.getId() != 0);
 		em.getTransaction().commit();
 		em.close();
-		
-		//Create Data
+
+		// Create Data
 		em = emf.createEntityManager();
 		em.getTransaction().begin();
-		
-		Therapy therapy = new Therapy("test",patient);
-		TherapyResult therapyResult = new TherapyResult("test",data, therapy);
-		
+
+		Therapy therapy = new Therapy("test", patient);
+		TherapyResult therapyResult = new TherapyResult("test", data, therapy);
+
 		em.persist(therapy);
 		em.persist(therapyResult);
-		
+
 		em.getTransaction().commit();
 		em.close();
-		
+
+		// Remove Data, Patient and Sensor
+		removeTherapy(therapy, patient);
+		removeData(data, patient, sensor);
 		removePatient(patient);
 		removeSensor(sensor);
 	}
--- de.lmu.ifi.dbs.medmon.derby/src/test/java/de/lmu/ifi/dbs/medmon/database/install/DatabaseTest.java
+++ de.lmu.ifi.dbs.medmon.derby/src/test/java/de/lmu/ifi/dbs/medmon/database/install/DatabaseTest.java
@@ -4,13 +4,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
-import java.util.List;
+import java.util.Properties;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,139 +23,132 @@ import de.lmu.ifi.dbs.medmon.database.entity.Data;
 import de.lmu.ifi.dbs.medmon.database.entity.Patient;
 import de.lmu.ifi.dbs.medmon.database.entity.Sensor;
 import de.lmu.ifi.dbs.medmon.database.entity.Therapy;
-import de.lmu.ifi.dbs.medmon.database.entity.TherapyResult;
 
 public class DatabaseTest {
 
-    protected static EntityManagerFactory emf;
+	protected static EntityManagerFactory emf;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        emf = Persistence.createEntityManagerFactory("medmon");
+	@BeforeClass
+	public static void setUp() throws Exception {
+		// Set preference node
+		Path dbRoot = Paths.get(System.getProperty("user.dir"), "target", "test-db");
+		trace("Test-DB root path is at " + dbRoot);
+		Files.createDirectories(dbRoot);
+		ConfigurationScope.INSTANCE.getNode("medmon").put("patient", dbRoot.resolve("homefolder").toString());
+		Properties properties = new Properties();
+		properties.put("javax.persistence.jdbc.url", "jdbc:derby:" + dbRoot.resolve("db") + ";create=true");
+		emf = Persistence.createEntityManagerFactory("medmon", properties);
+	}
 
-        // Remove all patients/sensors
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+	@AfterClass
+	public static void tearDown() throws Exception {
+		emf.close();
+	}
 
-        List<Patient> patients = em.createNamedQuery("Patient.findAll").getResultList();
-        for (Patient patient : patients) {
-            em.remove(patient);
-        }
+	@Test
+	public void testEntitManagerFactory() {
+		assertNotNull(getClass().getClassLoader().getResource("META-INF/persistence.xml"));
+		assertNotNull(emf);
+		EntityManager em = emf.createEntityManager();
+		assertNotNull(em);
+		em.close();
+	}
 
-        List<Sensor> sensors = em.createNamedQuery("Sensor.findAll").getResultList();
-        for (Sensor sensor : sensors) {
-            em.remove(sensor);
-        }
+	protected Sensor createSensor() {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Sensor sensor = new Sensor("TestSensor", "TestSensor:1.0.0", "1.0.0", "txt");
+		em.persist(sensor);
+		em.getTransaction().commit();
+		em.close();
+		trace("created sensor " + sensor);
+		return sensor;
+	}
 
-        List<Therapy> therapies = em.createNamedQuery("Therapy.findAll").getResultList();
-        for (Therapy therapy : therapies) {
-            em.remove(therapy);
-        }
+	protected boolean removeSensor(Sensor sensor) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Sensor s = em.find(Sensor.class, sensor.getId());
+		assertNotNull("Sensor not persisted", s);
+		em.remove(s);
+		em.getTransaction().commit();
+		em.close();
+		trace("removed sensor " + s);
+		return true;
+	}
 
-        List<TherapyResult> therapyResults = em.createNamedQuery("TherapyResult.findAll").getResultList();
-        for (TherapyResult therapyResult : therapyResults) {
-            em.remove(therapyResult);
-        }
+	protected Patient createPatient() {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Patient patient = new Patient("John", "Smith", "123A" + Math.random());
+		patient.setInsuranceId("xyz123");
+		em.persist(patient);
+		em.getTransaction().commit();
+		em.close();
+		trace("created patient " + patient);
+		return patient;
+	}
 
-        em.getTransaction().commit();
-        em.close();
+	protected boolean removePatient(Patient patient) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Patient p = em.find(Patient.class, patient.getId());
+		assertNotNull("Patient not persisted", p);
 
-    }
+		em.remove(p);
+		em.getTransaction().commit();
+		em.close();
+		trace("created patient " + p);
+		return true;
+	}
 
-    @AfterClass
-    public static void tearDown() throws Exception {
-        emf.close();
-    }
+	protected Data createData(Patient patient, Sensor sensor) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Data data = new Data(new Date(0), new Date(10), sensor);
 
-    @Test
-    public void testEntitManagerFactory() {
-        assertNotNull(getClass().getClassLoader().getResource("META-INF/persistence.xml"));
-        assertNotNull(emf);
-        EntityManager em = emf.createEntityManager();
-        assertNotNull(em);
-        em.close();
-    }
+		assertEquals("Id should not be set", data.getId(), 0);
+		em.persist(data);
+		assertTrue("Id should be set", data.getId() != 0);
+		em.getTransaction().commit();
+		em.close();
+		trace("created data " + data + " for patient " + patient + ", sensor " + sensor);
+		return data;
+	}
 
-    protected Sensor createSensor() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Sensor sensor = new Sensor("TestSensor", "TestSensor:1.0.0", "1.0.0", "txt");
-        em.persist(sensor);
-        em.getTransaction().commit();
-        em.close();
-        trace("created sensor " + sensor);
-        return sensor;
-    }
+	protected boolean removeData(Data data, Patient patient, Sensor sensor) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Data d = em.find(Data.class, data.getId());
+		assertNotNull("Data not persisted!", d);
+		assertNotNull("Relation to Sensor not persisted", d.getSensor());
 
-    protected boolean removeSensor(Sensor sensor) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Sensor s = em.find(Sensor.class, sensor.getId());
-        assertNotNull("Sensor not persisted", s);
-        em.remove(s);
-        em.getTransaction().commit();
-        em.close();
-        trace("removed sensor " + s);
-        return true;
-    }
+		assertEquals("Sensors not equally", d.getSensor(), sensor);
 
-    protected Patient createPatient() {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Patient patient = new Patient("John", "Smith", "123A" + Math.random());
-        patient.setInsuranceId("xyz123");
-        em.persist(patient);
-        em.getTransaction().commit();
-        em.close();
-        trace("created patient " + patient);
-        return patient;
-    }
+		em.remove(d);
+		em.getTransaction().commit();
+		em.close();
+		trace("removed data " + data);
+		return true;
+	}
 
-    protected boolean removePatient(Patient patient) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Patient p = em.find(Patient.class, patient.getId());
-        assertNotNull("Patient not persisted", p);
+	protected void removeTherapy(Therapy therapy, Patient patient) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Therapy t = em.find(Therapy.class, therapy.getId());
+		assertNotNull("Therapy not persisted!", t);
+		assertNotNull("Relation to Patient not persisted", t.getPatient());
 
-        em.remove(p);
-        em.getTransaction().commit();
-        em.close();
-        trace("created patient " + p);
-        return true;
-    }
+		assertEquals("Patients not equally", t.getPatient(), patient);
 
-    protected Data createData(Patient patient, Sensor sensor) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Data data = new Data(new Date(0), new Date(10), sensor);
+		em.remove(t);
+		em.getTransaction().commit();
+		em.close();
+		trace("removed therapy " + therapy);
+	}
 
-        assertEquals("Id should not be set", data.getId(), 0);
-        em.persist(data);
-        assertTrue("Id should be set", data.getId() != 0);
-        em.getTransaction().commit();
-        em.close();
-        trace("created data " + data + " for patient " + patient + ", sensor " + sensor);
-        return data;
-    }
-
-    protected boolean removeData(Data data, Patient patient, Sensor sensor) {
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        Data d = em.find(Data.class, data.getId());
-        assertNotNull("Data not persisted!", d);
-        assertNotNull("Relation to Sensor not persisted", d.getSensor());
-
-        assertEquals("Sensors not equally", d.getSensor(), sensor);
-
-        em.remove(d);
-        em.getTransaction().commit();
-        em.close();
-        trace("removed data " + data);
-        return true;
-    }
-
-    protected void trace(String msg) {
-        System.out.println("[DEBUG] " + msg);
-    }
+	protected static void trace(String msg) {
+		System.out.println("[DEBUG] " + msg);
+	}
 
 }
